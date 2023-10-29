20c41eb03d49644b673231a536d3238c52587eb0
uwjpt <uwjpt@student.kit.edu>
2023-10-29 16:58:01 +0100

    Add path

--- jcmsuite/sphere/run.py
+++ jcmsuite/sphere/run.py
@@ -1,12 +1,12 @@
 """Template for a T-matrix calculation with JCMsuite at the example of a sphere."""
 
 import os
-
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import h5py
 import jcmwave
 import meshparser
 import numpy as np
-
 import tmatrix_tools
 
 
